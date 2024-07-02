95104b24cb62f802e4c76553b24df0d11f544cd9
camdentadhg <camdent@gmail.com>
2024-07-02 12:02:49 -0500

    shuffle the deck

--- CardApp/src/App.jsx
+++ CardApp/src/App.jsx
@@ -13,14 +13,5 @@ function App() {
 export default App
 
 
-//add shuffle the deck functionality
-  //button for shuffling the deck
-  //on click, shuffles the deck
-  //button inactive during api call
-      //variable set to false right before api call
-      // set to true right after api call
-      //use ternary operator
-  //remove all cards from the screen
-
 //style the app
 //add timer functionality
--- CardApp/src/Deck.jsx
+++ CardApp/src/Deck.jsx
@@ -6,13 +6,12 @@ const Deck = () => {
     const [deckId, setDeckId] = useState(null);
     const [count, setCount] = useState(0);
     const [cards, setCards] = useState([]);
-    const [drawDisabled, setDrawDisabled] = useState(false);
+    const [shuffle, setShuffle] = useState(false);
 
     useEffect(() => {
         async function getDeck(){
             const response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
             setDeckId(response.data.deck_id);
-            console.log(response.data.deck_id);
         }
         getDeck();
     }, []);
@@ -21,10 +20,14 @@ const Deck = () => {
         setCount(c => c + 1);
     }
 
+    const toggleShuffle = () => {
+        setShuffle(true);
+    }
+
     useEffect(() => {
         async function drawCard(){
+            console.log(deckId);
             const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`);
-            console.log(response);
             if (!response.data.success){
                 alert('Error: no more cards');
             }
@@ -32,15 +35,27 @@ const Deck = () => {
             const newArray = [...cards, newCard];
             setCards(newArray);
         }
-        console.log('count = ', count);
         if (deckId && count > 0){
             drawCard();
         }
-    }, [count])
+    }, [count]);
+
+    useEffect(() => {
+        async function shuffleDeck(){
+            console.log(deckId);
+            setCards([]);
+            const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle`);
+        }
+        if (shuffle === true){
+            shuffleDeck();
+            setShuffle(false);
+        }
+    }, [shuffle]);
 
     return (
         <div>
             <button  onClick={incrementCount}>Draw a card</button>
+            <button onClick={toggleShuffle} disabled={shuffle}>Shuffle the deck</button>
             <div>
                 {cards.map((card, i) => (
                     <Card key={i} image={card.image} altText={card.card}/>
